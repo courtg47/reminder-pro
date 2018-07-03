1e61ef1edd5e828bf91b2045897febf618927c26
Courtney Green <courtg47@gmail.com>
2018-07-03 15:00:40 -0500

    feat: Add props and finish Redux setup of app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    this.props.addReminder(this.state.text);
+  }
+
   render() {
     return (
       <section className="app">
         <h1 className="title">Reminder Pro</h1>
         <section className="form-inline">
           <section className="form-group">
-            <input clasName="form-control" placeholder="I have to..." />
+            <input className="form-control" placeholder="I have to..." onChange={event => this.setState({text: event.target.value})}/>
           </section>
-          <button type="button" className="btn btn-success">Add</button>
+          <button type="button" className="btn btn-success" onClick={() => this.addReminder}>Add</button>
         </section>
       </section>
     )
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, { addReminder })(App);
