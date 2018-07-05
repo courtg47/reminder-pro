55cb4fb7fcc9d24d6570d436884a3860241ff0cf
Courtney Green <courtg47@gmail.com>
2018-07-05 15:53:40 -0500

    feat: Delete all reminders saved with a Delete All button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder, deleteReminder } from '../actions';
+import { addReminder, deleteReminder, clearReminders } from '../actions';
 import moment from 'moment';
 
 class App extends Component {
@@ -55,6 +55,7 @@ class App extends Component {
           <button type="button" className="btn btn-success" onClick={() => this.addReminder()}>Add</button>
         </section>
         { this.renderReminders() }
+        <section className="btn btn-danger" onClick={() => this.props.clearReminders()}>Delete All</section>
       </section>
     )
   }
@@ -66,4 +67,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App);
